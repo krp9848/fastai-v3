1f032b5ee3f930e9657cc7258c0cb06f36a1ad05
krp9848 <kabirajpant@gmail.com>
2019-12-07 23:23:55 +0530

    updated url and class variable

--- app/server.py
+++ app/server.py
@@ -9,10 +9,12 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1U7IrZJzixn_7qVERKQYCodb6XV88aOG8'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+# classes = ['black', 'grizzly', 'teddys']
+classes = ['cristiano_ronaldo','neymar', 'lionel_messi', 'david_beckham', 'james_rodriguez','ronaldinho','gareth_bale','zlatan_ibrahimovic','marcelo','luis_suarez']
+
 path = Path(__file__).parent
 
 app = Starlette()
